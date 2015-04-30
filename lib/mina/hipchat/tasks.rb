f03610b2f5ec974d56942b8959ce5aafbb14e35c
Jakub Juszczak <netghost03@gmail.com>
2015-04-30 19:00:21 +0200

    Add starting and ending tasks

--- lib/mina/hipchat/tasks.rb
+++ lib/mina/hipchat/tasks.rb
@@ -6,14 +6,55 @@ require 'uri'
 
 namespace :hipchat do
 
+	task :starting do
+		if hipchat_token and hipchat_room
+			starting_msg = "#{deployer} is deploying now #{announced_application_name}"
+
+			# Post starting message
+			post_hipchat_message(starting_msg)
+
+			# Start time tracking
+			set(:start_time, Time.now)
+		else
+			print_local_status "Unable to send notification, no HipChat details provided."
+		end
+	end
+
+	task :finished do
+		if hipchat_token and hipchat_room
+
+			# End time tracking and calculate elapsed time
+			end_time = Time.now
+			start_time = fetch(:start_time)
+			elapsed = end_time.to_i - start_time.to_i
+
+		      end_msg = "#{deployer} successfully deployed #{announced_application_name} in #{elapsed} seconds."
+		      post_hipchat_message(end_msg);
+	      else
+			print_local_status "Unable to send notification, no HipChat details provided."
+		end
+
+	end
+
+	def short_revision
+		deployed_revision[0..7] if deployed_revision
+	end
+
+	def announced_application_name
+		"".tap do |output|
+			output << hipchat_application if hipchat_application
+			output << " #{branch}" if branch
+			output << " (#{short_revision})" if short_revision
+		end
+	end
+
 	def post_hipchat_message(message)
 		# Parse the URI and handle the https connection
-		uri = URI.parse("https://api.hipchat.com/v2/room/#{room_id}/notification?auth_token=#{hipchat_token})
+		uri = URI.parse("https://api.hipchat.com/v2/room/#{room_id}/notification?auth_token=#{hipchat_token}")
 		http = Net::HTTP.new(uri.host, uri.port)
 
 		payload = {
-			"parse"       			=> "full",
-			"id_or_roomname" 	=> slack_room,
+			"id_or_roomname" 	=> hipchat_room,
 			"color"   	 			=> color,
 			"message"  			=> message,
 			"notify"  				=> notify,
