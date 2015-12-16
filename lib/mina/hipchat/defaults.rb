953a41ca7c042bb55515ac9c396c7e8ed1fed478
Nikita Tarasov <nikita@mygento.ru>
2015-12-16 17:14:09 +0300

    formatting

--- lib/mina/hipchat/defaults.rb
+++ lib/mina/hipchat/defaults.rb
@@ -1,14 +1,12 @@
 # Required
-set_default :hipchat_token, 		-> { ENV['HIPCHAT_TOKEN'] }
-set_default :hipchat_room, 			-> { ENV['HIPCHAT_ROOM'] }
+set_default :hipchat_token,         -> { ENV['HIPCHAT_TOKEN'] }
+set_default :hipchat_room,          -> { ENV['HIPCHAT_ROOM'] }
 
 # Optional
-set_default :hipchat_color,             -> { ENV['HIPCHAT_COLOR'] || 'yellow'}
-set_default :hipchat_application,       -> { ENV['HIPCHAT_APPLICATION'] || application }
-set_default :hipchat_notify,            -> { ENV['HIPCHAT_NOTIFY'] || false }
-
-
+set_default :hipchat_color,         -> { ENV['HIPCHAT_COLOR'] || 'yellow'}
+set_default :hipchat_application,   -> { ENV['HIPCHAT_APPLICATION'] || application }
+set_default :hipchat_notify,        -> { ENV['HIPCHAT_NOTIFY'] || false }
 
 # Git
-set_default :deployer,          		-> { ENV['GIT_AUTHOR_NAME'] || %x[git config user.name].chomp }
-set_default :deployed_revision, 		-> { ENV['GIT_COMMIT'] || %x[git rev-parse #{branch}].strip }
+set_default :deployer,              -> { ENV['GIT_AUTHOR_NAME'] || %x[git config user.name].chomp }
+set_default :deployed_revision,     -> { ENV['GIT_COMMIT'] || %x[git rev-parse #{branch}].strip }
